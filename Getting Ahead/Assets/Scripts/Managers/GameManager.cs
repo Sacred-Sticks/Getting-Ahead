670b6b7e32c492d73b6a0d1f68845bf92c69fc6a
Lucas Ackman <AckmanLD@gmail.com>
2023-10-02 11:28:50 -0500

    Turned GameManager into Singleton

--- Getting Ahead/Assets/Scripts/Managers/GameManager.cs
+++ Getting Ahead/Assets/Scripts/Managers/GameManager.cs
@@ -5,9 +5,22 @@ public class GameManager : MonoBehaviour
 {
     [SerializeField] private InputManager inputManager;
 
+    public static GameManager instance;
+
     private void Awake()
     {
+        InitializeSingleton();
+
         inputManager.Initialize();
+    }
+
+    private void InitializeSingleton()
+    {
+
+        if (instance != null)
+            Destroy(gameObject);
+        instance = this;
+        DontDestroyOnLoad(this);
     }
 
 }
