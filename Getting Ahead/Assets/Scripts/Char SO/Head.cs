fce3156aee23e1b12874c04270568a8cb5bb80d9
Lucas Ackman <AckmanLD@gmail.com>
2023-10-05 18:53:14 -0500

    Add Head SO's
    Contains communal information, can be added to later if needed. Not currently implemented to affect anything.

---
+++ Getting Ahead/Assets/Scripts/Char SO/Head.cs
@@ -0,0 +1,54 @@
+using UnityEngine;
+
+public class Head : ScriptableObject
+{
+    [SerializeField] private float moveSpeed;
+    [SerializeField] private float attackRate;
+    [SerializeField] private float attackDamage;
+    [SerializeField] private float health;
+    [SerializeField] private float defense;
+    [SerializeField] private float iframes;
+
+    public float MoveSpeed
+    {
+        get
+        {
+            return moveSpeed;
+        }
+    }
+    public float AttackRate
+    {
+        get
+        {
+            return attackRate;
+        }
+    }
+    public float AttackDamage
+    {
+        get
+        {
+            return attackDamage;
+        }
+    }
+    public float Health
+    {
+        get
+        {
+            return health;
+        }
+    }
+    public float Defense
+    {
+        get
+        {
+            return defense;
+        }
+    }
+    public float Iframes
+    {
+        get
+        {
+            return iframes;
+        }
+    }
+}
