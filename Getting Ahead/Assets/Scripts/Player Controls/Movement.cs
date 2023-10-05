ef8a51f28acb1acd6d9dd349b172453455b56e2d
Brandon M <75460543+GelingenTech@users.noreply.github.com>
2023-10-05 14:56:04 -0500

    First Implementation of Movement
    Basic character movement. Code can be found in the Movement script.

---
+++ Getting Ahead/Assets/Scripts/Player Controls/Movement.cs
@@ -0,0 +1,39 @@
+using UnityEngine;
+using Kickstarter.Identification;
+using Kickstarter.Inputs;
+
+[RequireComponent(typeof(Player))]
+[RequireComponent(typeof(Rigidbody))]
+public class Movement : MonoBehaviour
+{
+    [SerializeField]
+    private float moveSpeed;
+    [SerializeField]
+    private Vector2Input movementInput;
+    private Vector2 rawDirection;
+    private Player player;
+    private Rigidbody rb;
+
+    private void Awake()
+    {
+        player = GetComponent<Player>();
+        rb = GetComponent<Rigidbody>();
+    }
+
+    private void Start()
+    {
+        movementInput.SubscribeToInputAction(RecieveInput, player.PlayerID);
+    }
+
+    private void FixedUpdate()
+    {
+        var movementDirection = new Vector3(rawDirection.x, 0f, rawDirection.y);
+        rb.velocity = movementDirection * moveSpeed;
+        Debug.Log(rb.velocity.magnitude);
+    }
+
+    private void RecieveInput(Vector2 input)
+    {
+        rawDirection = input;
+    }
+}
