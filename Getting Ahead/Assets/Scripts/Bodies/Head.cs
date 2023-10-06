5373397dd2e30a8e92ada2c5e1d51a12000fd741
Lucas Ackman <AckmanLD@gmail.com>
2023-10-06 17:19:34 -0500

    Add Head Base Class

---
+++ Getting Ahead/Assets/Scripts/Bodies/Head.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+
+[RequireComponent(typeof(Movement))]
+public class Head : MonoBehaviour
+{
+    [SerializeField] private CharacterStatistics characterStatistics;
+
+    private Movement movement;
+
+    private void Awake()
+    {
+        movement = GetComponent<Movement>();
+    }
+
+    private void Start()
+    {
+        
+    }
+}
--- Getting Ahead/Assets/Scripts/SO's/Characters/CharacterStatistics.cs
+++ Getting Ahead/Assets/Scripts/SO's/Characters/CharacterStatistics.cs
@@ -1,6 +1,7 @@
 using UnityEngine;
 
-public class Head : ScriptableObject
+[CreateAssetMenu(fileName = "Character Stats", menuName = "Getting Ahead/Character Stats")]
+public class CharacterStatistics : ScriptableObject
 {
     [SerializeField] private float moveSpeed;
     [SerializeField] private float attackRate;
