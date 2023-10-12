9805d823e0fe5013b9995934f95047db8ff71400
Andrinor1 <andrinor2001@gmail.com>
2023-10-11 19:49:24 -0500

    Camera Script Update
    The camera script will now grab all components with the tag VirtualCamera within the scene and add them into an array, which we can use to auto-populate an array based on the number of rooms.

--- Getting Ahead/Assets/Scenes/GRIFFIN/Scripts/Camera Script.cs
+++ Getting Ahead/Assets/Scenes/GRIFFIN/Scripts/Camera Script.cs
@@ -4,17 +4,20 @@ using Cinemachine;
 using Kickstarter.Inputs;
 using UnityEngine;
 using Kickstarter.Identification;
+using System.Linq;
 
 public class CameraScript : MonoBehaviour
 {
     [SerializeField] CinemachineVirtualCamera vcam1;
     [SerializeField] CinemachineVirtualCamera vcam2;
     [SerializeField] FloatInput CameraInput;
+    private GameObject[] CameraObjects;
     void Start()
     {
         CameraInput.SubscribeToInputAction(ReceiveInput, Player.PlayerIdentifier.KeyboardAndMouse);
         vcam2.m_Priority = 1;
         vcam1.m_Priority = 2;
+        CameraObjects = GameObject.FindGameObjectsWithTag("VirtualCamera");
     }
 
     void ReceiveInput(float input)
@@ -22,12 +25,12 @@ public class CameraScript : MonoBehaviour
         switch (input)
         {
             case 1:
-                vcam2.m_Priority = 0;
-                vcam1.m_Priority = 9;
+                CameraObjects[0].GetComponent<CinemachineVirtualCamera>().m_Priority = 9;
+                CameraObjects[1].GetComponent<CinemachineVirtualCamera>().m_Priority = 0;
                 break;
             case -1:
-                vcam1.m_Priority = 0;
-                vcam2.m_Priority = 9;
+                CameraObjects[1].GetComponent<CinemachineVirtualCamera>().m_Priority = 9;
+                CameraObjects[0].GetComponent<CinemachineVirtualCamera>().m_Priority = 0;
                 break;
         }
     }
