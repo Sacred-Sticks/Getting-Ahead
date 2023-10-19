0dae43178c5b4be924552578d340d7a7d37356f0
Lucas Ackman <AckmanLD@gmail.com>
2023-10-19 11:11:56 -0500

    Update to Kickstarter 1.3.1

--- Getting Ahead/Assets/Kickstarter/Inputs/IInputReceiver.cs
+++ Getting Ahead/Assets/Kickstarter/Inputs/IInputReceiver.cs
@@ -1,8 +1,11 @@
 using Kickstarter.Identification;
 
-public interface IInputReceiver
+namespace Kickstarter.Inputs
 {
-    public void SubscribeToInputs(Player player);
+    public interface IInputReceiver
+    {
+        public void SubscribeToInputs(Player player);
 
-    public void UnsubscribeToInputs(Player player);
+        public void UnsubscribeToInputs(Player player);
+    }
 }
