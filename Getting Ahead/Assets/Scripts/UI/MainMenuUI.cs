b1e7cac2813c3d90dafe3a145b11d91be6c3d767
Andrinor1 <andrinor2001@gmail.com>
2023-12-03 11:40:17 -0600

    Remade the Main Menu
    No function for Controls or Credits

--- Getting Ahead/Assets/Scripts/UI/MainMenuUI.cs
+++ Getting Ahead/Assets/Scripts/UI/MainMenuUI.cs
@@ -1,4 +1,3 @@
-using System;
 using UnityEngine;
 using UnityEngine.UIElements;
 
@@ -7,12 +6,16 @@ public class MainMenuUI : MonoBehaviour
     [SerializeField] private GameManager gameManager;
     Button buttonPlay;
     Button buttonQuit;
+    Button buttonCredits;
+    Button buttonControls;
     private void OnEnable()
     {
         VisualElement root = GetComponent<UIDocument>().rootVisualElement;
 
         buttonPlay = root.Q<Button>("PlayButton");
         buttonQuit = root.Q<Button>("QuitButton");
+        buttonCredits = root.Q<Button>("CreditButton");
+        buttonControls = root.Q<Button>("ControlButton");
 
         buttonPlay.RegisterCallback<ClickEvent>((evt) => gameManager.ChangeScene("play"));
         buttonQuit.RegisterCallback<ClickEvent>((evt) => Application.Quit());
@@ -24,12 +27,17 @@ public class MainMenuUI : MonoBehaviour
         buttonPlay.RegisterCallback<FocusOutEvent>(OnFocusOut);
         buttonQuit.RegisterCallback<FocusInEvent>(OnFocusIn);
         buttonQuit.RegisterCallback<FocusOutEvent>(OnFocusOut);
+        buttonControls.RegisterCallback<FocusInEvent>(OnFocusIn);
+        buttonControls.RegisterCallback<FocusOutEvent>(OnFocusOut);
+        buttonCredits.RegisterCallback<FocusInEvent>(OnFocusIn);
+        buttonCredits.RegisterCallback<FocusOutEvent>(OnFocusOut);
 
         buttonPlay.RegisterCallback<NavigationMoveEvent>((evt) =>
         {
             switch (evt.direction)
             {
-                case NavigationMoveEvent.Direction.Down: buttonQuit.Focus(); break;
+                case NavigationMoveEvent.Direction.Down: buttonCredits.Focus(); break;
+                case NavigationMoveEvent.Direction.Right: buttonControls.Focus(); break;
             }
             evt.PreventDefault();
         });
@@ -37,7 +45,26 @@ public class MainMenuUI : MonoBehaviour
         {
             switch (evt.direction)
             {
+                case NavigationMoveEvent.Direction.Up: buttonControls.Focus(); break;
+                case NavigationMoveEvent.Direction.Left: buttonCredits.Focus(); break;
+            }
+            evt.PreventDefault();
+        });
+        buttonControls.RegisterCallback<NavigationMoveEvent>((evt) =>
+        {
+            switch (evt.direction)
+            {
+                case NavigationMoveEvent.Direction.Down: buttonQuit.Focus(); break;
+                case NavigationMoveEvent.Direction.Left: buttonPlay.Focus(); break;
+            }
+            evt.PreventDefault();
+        });
+        buttonCredits.RegisterCallback<NavigationMoveEvent>((evt) =>
+        {
+            switch (evt.direction)
+            {
                 case NavigationMoveEvent.Direction.Up: buttonPlay.Focus(); break;
+                case NavigationMoveEvent.Direction.Right: buttonQuit.Focus(); break;
             }
             evt.PreventDefault();
         });
